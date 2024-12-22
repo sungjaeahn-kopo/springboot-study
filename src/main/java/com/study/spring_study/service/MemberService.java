f9f207216cda97d5303331d37ff992cde627c001
sungjae_ahn <sungjae_ahn2@tmax.co.kr>
2024-12-22 21:10:02 +0900

    Spring basic, MemberService

---
+++ src/main/java/com/study/spring_study/service/MemberService.java
@@ -0,0 +1,44 @@
+package com.study.spring_study.service;
+
+import com.study.spring_study.domain.Member;
+import com.study.spring_study.repository.MemberRepository;
+import com.study.spring_study.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원가입
+     * @param member
+     * @return
+     */
+    public Long join(Member member) {
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName()).ifPresent(
+            m -> {
+                throw new IllegalStateException("이미 존재하는 회원입니다.");
+            }
+        );
+    }
+
+    /**
+     * 전체 회원 조회
+     * @return
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
